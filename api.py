729e6b2caf149f3fecb50d47820d45f0f540c507
= <=>
2020-03-23 09:21:57 +0530

    Updated scraper to incorporate website changes

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, escape, request, jsonify
 from scraper import web_scrape
+
 app = Flask(__name__)
 
 
@@ -22,4 +23,4 @@ def api():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- scraper.py
+++ scraper.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 def web_scrape():
     dfs = pd.read_html('https://www.mohfw.gov.in/')
-    return dfs[0]
+    return dfs[1]
+
+
+if __name__ == "__main__":
+    web_scrape()
